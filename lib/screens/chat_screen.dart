ceb3524378f4d6e674c4fdcedea0b86d6c4351ba
your-githb-usernam <midomohamed61>
2024-01-21 07:52:10 +0200

    third commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,60 +12,73 @@ class ChatScreen extends StatefulWidget {
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 CollectionReference messages = FirebaseFirestore.instance.collection('messages');
+TextEditingController controller = TextEditingController();
 
 
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: KPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(KLogo,
-            height: 50,
-            ),
-            Text('Scholar chat'),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(itemBuilder: (context,index){
-              return ChatBubble();
-            }),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: TextField(
-              onSubmitted: (data){
-                messages.add({
-                  'message':data
-                });
-              },
-              decoration: InputDecoration(
-                hintText: 'Send Message',
-                suffixIcon: Icon(Icons.send,
-                  color: KPrimaryColor,
+    return  FutureBuilder<QuerySnapshot>(
+        future: messages.get(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor: KPrimaryColor,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(KLogo,
+                      height: 50,
+                    ),
+                    Text('Scholar chat'),
+                  ],
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16)
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: KPrimaryColor,
-                  )
-                ),
+                centerTitle: true,
               ),
-            ),
-          )
-        ],
-      ),
+              body: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(itemBuilder: (context,index){
+                      return ChatBubble();
+                    }),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: TextField(
+                      controller: controller,
+                      onSubmitted: (data){
+                        messages.add({
+                          'message':data
+                        });
+                        controller.clear();
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Send Message',
+                        suffixIcon: Icon(Icons.send,
+                          color: KPrimaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color: KPrimaryColor,
+                            )
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+          else{
+            Text('Loading');
+          }
+        }
     );
   }
 }
